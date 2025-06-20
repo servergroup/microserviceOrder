9c6edd05db77517a83870eab18d2aaa2309414c3
servergroup <macagninoriccardo85@gmail.com>
2025-06-20 13:10:30 +0200

    fafaf

--- src/main/java/it/apuliadigital/OrderService/controller/OrderController.java
+++ src/main/java/it/apuliadigital/OrderService/controller/OrderController.java
@@ -1,5 +1,38 @@
 package it.apuliadigital.OrderService.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.apuliadigital.OrderService.Service.IOrder;
+import it.apuliadigital.OrderService.model.Order;
+import jakarta.persistence.EntityNotFoundException;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
-    
+
+    @Autowired
+    private IOrder orderService;
+
+    @PostMapping("/addOrders")
+
+    public boolean createOrder(@RequestBody Order order) {
+        return orderService.saveOrder(order);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable int id) {
+        try {
+            orderService.deleteOrder(id);
+            return ResponseEntity.noContent().build(); // 204 No Content
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+    }
 }
